27b919e5a3ca10f757190381debeb9eaf7b50031
rawrxhr <176803592+rawrxhr@users.noreply.github.com>
2026-09-16 00:56:09 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d1={}
+        d2={}
+        if(len(s)!=len(t)):
+            return False
+        for i in s:
+            if i not in d1:
+                d1[i]=1
+            else:
+                d1[i]=1+d1[i]
+        for i in t:
+            if i in d2:
+                d2[i]=1+d2[i]
+            else:
+                d2[i]=1
+        if d1!=d2:
+            return False
+        return True
+
+
+
+        
+        
+        
